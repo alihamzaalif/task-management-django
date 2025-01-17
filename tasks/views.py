a30f1ea79d45d15b4c2a6c5171dacab500254ec0
Hamza <alihamzaalif@gmail.com>
2025-01-17 14:16:54 +0600

    Forms and Mixins

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.forms import TaskForm, TaskModelForm
+from tasks.models import Employee, Task
 
 # Create your views here.
 def home(request):
@@ -34,4 +36,50 @@ def test(request):
     }
     return render(request,'test.html',context)
 
+def create_task(request):
+    employees = Employee.objects.all()
+    # form = TaskForm(employees=employees) #For GET
+    form = TaskModelForm() #For GET
+
+    # if request.method == "POST":
+    #     form = TaskForm(request.POST,employees=employees)
+    #     if form.is_valid():
+    #         # print(form.cleaned_data)
+    #         data = form.cleaned_data
+    #         title = data.get('title')
+    #         description = data.get('description')
+    #         due_date = data.get('due_date')
+    #         assigned_to = data.get('assigned_to')
+    #         task = Task.objects.create(title=title, description=description, due_date=due_date)
+    #         #Assign employee to task
+    #         for emp_id in assigned_to:
+    #             employee = Employee.objects.get(id=emp_id)
+    #             task.assigned_to.add(employee)
+            
+    #         return HttpResponse("Task Added sucessfully")
+    if request.method == "POST":
+        form = TaskModelForm(request.POST)
+        if form.is_valid():
+            """For Model Form Data"""
+            print(form)
+            form.save()
+            return render(request,'task_form.html',{"form":form,"message": "task added successfully"})
+            # print(form.cleaned_data)
+            """ For Django Form data"""
+            # data = form.cleaned_data
+            # title = data.get('title')
+            # description = data.get('description')
+            # due_date = data.get('due_date')
+            # assigned_to = data.get('assigned_to')
+            # task = Task.objects.create(title=title, description=description, due_date=due_date)
+            # #Assign employee to task
+            # for emp_id in assigned_to:
+            #     employee = Employee.objects.get(id=emp_id)
+            #     task.assigned_to.add(employee)
+            # 
+            # return HttpResponse("Task Added sucessfully")
+    context = {
+        "form":form
+    }
+    return render(request,"task_form.html",context)
     
