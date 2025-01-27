c37b70f75faa4779e3ff9405fa5b03d35561a295
Hamza <alihamzaalif@gmail.com>
2025-01-27 21:19:49 +0600

    Login

--- tasks/forms.py
+++ tasks/forms.py
@@ -16,6 +16,10 @@ class TaskForm(forms.Form):
 
 class StyledFormMixin:
     """Mixins to appply style to form"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_styled_widgets()
+
     default_classes = "border-2 border-gray-300 w-full p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500 hover:border-rose-500"
 
     def apply_styled_widgets(self):
