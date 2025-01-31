fd19f43921c60b6e0c9af9d45129c909916e17eb
Hamza <alihamzaalif@gmail.com>
2025-01-31 19:12:24 +0600

    authentication done

--- tasks/views.py
+++ tasks/views.py
@@ -5,25 +5,19 @@ from tasks.models import Employee, Task, TaskDetail, Project
 from datetime import date
 from django.db.models import Q, Count, Max, Min, Avg
 from django.contrib import messages
-# Create your views here.
+from django.contrib.auth.decorators import user_passes_test, permission_required, login_required
 
+
+# Create your views here.
+def is_manager(user):
+    return user.groups.filter(name='Manager').exists()
+
+def is_employee(user):
+    return user.groups.filter(name='Employee').exists()
+
+@user_passes_test(is_manager, login_url='no-permission')
 def manager_dashboard(request):
     type = request.GET.get('type', 'all') #provides reply 'all' if nothing is provided
-    # print(type)
-    
-
-    #getting task count
-    # total_task = tasks.count()
-    # completed_task = tasks.filter(status="COMPLETED").count()
-    # in_progress_task = tasks.filter(status = "IN_PROGRESS").count()
-    # pending_task = tasks.filter(status = "PENDING").count()
-    
-    # count = {
-    #     "total_task":,
-    #     "completed_task":,
-    #     "in_progress_task":,
-    #     "pending_task"
-    # }
     base_query = Task.objects.select_related('details').prefetch_related('assigned_to')
     counts = Task.objects.aggregate(
         total=Count('id'),
@@ -43,40 +37,19 @@ def manager_dashboard(request):
         tasks = base_query.all()
     context = {
         "tasks" : tasks,
-        "counts" : counts,
-        # "total_task" : total_task,
-        # "pending_task" : pending_task,
-        # "in_progress_task" : in_progress_task,
-        # "completed_task" : completed_task
+        "counts" : counts
     }
     return render(request,'dashboard/manager-dashboard.html', context)
 
-def user_dashboard(request):
+@user_passes_test(is_employee, login_url='no-permission')
+def employee_dashboard(request):
     return render(request,'dashboard/user-dashboard.html')
 
-
+@login_required
+@permission_required("tasks.add_task", login_url='no-permission')
 def create_task(request):
-    # employees = Employee.objects.all()
-    # form = TaskForm(employees=employees) #For GET
     task_form = TaskModelForm() #For GET
     task_detail_form = TaskDetailModelForm()
-
-    # if request.method == "POST":
-    #     form = TaskForm(request.POST,employees=employees)
-    #     if form.is_valid():
-    #         # print(form.cleaned_data)
-    #         data = form.cleaned_data
-    #         title = data.get('title')
-    #         description = data.get('description')
-    #         due_date = data.get('due_date')
-    #         assigned_to = data.get('assigned_to')
-    #         task = Task.objects.create(title=title, description=description, due_date=due_date)
-    #         #Assign employee to task
-    #         for emp_id in assigned_to:
-    #             employee = Employee.objects.get(id=emp_id)
-    #             task.assigned_to.add(employee)
-            
-    #         return HttpResponse("Task Added sucessfully")
     if request.method == "POST":
         task_form = TaskModelForm(request.POST) 
         task_detail_form = TaskDetailModelForm(request.POST)
@@ -90,26 +63,13 @@ def create_task(request):
 
             messages.success(request, "Task created successfully")
             return redirect('create-task')
-            # return render(request,'task_form.html',{"task_form":task_form, "task_detail_form":task_detail_form, "message": "task added successfully"})
-            # print(form.cleaned_data)
-            """ For Django Form data"""
-            # data = form.cleaned_data
-            # title = data.get('title')
-            # description = data.get('description')
-            # due_date = data.get('due_date')
-            # assigned_to = data.get('assigned_to')
-            # task = Task.objects.create(title=title, description=description, due_date=due_date)
-            # #Assign employee to task
-            # for emp_id in assigned_to:
-            #     employee = Employee.objects.get(id=emp_id)
-            #     task.assigned_to.add(employee)
-            # 
-            # return HttpResponse("Task Added sucessfully")
     context = {
         "task_form":task_form, "task_detail_form":task_detail_form
     }
     return render(request,"task_form.html",context)
 
+@login_required
+@permission_required("tasks.change_task", login_url='no-permission')
 def update_task(request,id):
     task = Task.objects.get(id=id)
     task_form = TaskModelForm(instance=task) #For GET
@@ -133,6 +93,8 @@ def update_task(request,id):
     }
     return render(request,"task_form.html",context)
 
+@login_required
+@permission_required("tasks.delete_task", login_url='no-permission')
 def delete_task(request,id):
     if request.method == 'POST':
         task = Task.objects.get(id=id)
@@ -141,40 +103,10 @@ def delete_task(request,id):
         return redirect('manager-dashboard')
     else:
         messages.error(request, "Something went wrong")
-    return redirect('manager-dashboard')
+        return redirect('manager-dashboard')
+
+@login_required
+@permission_required("tasks.view_task", login_url='no-permission')
 def view_task(request):
-    # # retrieve all data from task models
-    # tasks = Task.objects.all()
-
-    # # retrieve specific task
-    # task3 = Task.objects.get(pk=1)
-
-    # # fetch the first task
-    # first_task = Task.objects.first()
-
-    # pending_task = Task.objects.filter(status='PENDING')
-    # completed_task = Task.objects.filter(status='COMPLETED')
-    # today_pending = Task.objects.filter(due_date=date.today())
-
-    # task_prty = TaskDetail.objects.exclude(priority="L")
-
-    # return render(request, "show_task.html",{"tasks":tasks,"task3":task3,"first_task":first_task,"pending_task":pending_task,"completed_task":completed_task,"today_pending":today_pending,"task_prty":task_prty})
-
-    # SHOW TASKS THAT CONTAIN THE LETTER C AND STATUS PENDING
-    # tasks = Task.objects.filter(title__icontains="c",status="PENDING")
-
-    #SHOW T HE TASSKS THAHT ARE PENDING OR IN-PROGRESS
-
-    # tasks = Task.objects.filter(Q(status="PENDING") | Q(status="IN_PROGRESS"))
-    # tasks = Task.objects.filter(Q(status="PENDING") | Q(status="IN_PROGRESS")).exists()
-    # return render(request,'show_task.html',{"tasks":tasks})
-
-    #  reverse related
-    # tasks = Task.objects.select_related('details').all()
-    # tasks = TaskDetail.objects.select_related('task').all()
-    # tasks = Task.objects.select_related('project').all()
-
-    # Prefetch Related {Reverse Foreignkey, many to many}
-    # tasks = Project.objects.prefetch_related('task_set').all()
     tasks = Task.objects.prefetch_related('assigned_to').all()
     return render(request,"show_task.html",{"tasks":tasks})
--- users/views.py
+++ users/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth import login, authenticate, logout
 from  users.forms import RegisterForm, CustomRegistrationForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
-from users.forms import LoginForm
+from users.forms import LoginForm, AssignRoleForm, CreateGroupForm
 from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.decorators import user_passes_test, user_passes_test, login_required
+from django.db.models import Prefetch
 # Create your views here.
+
+def is_admin(user):
+    return user.groups.filter(name='Admin')
+
 def sign_up(request):
     if request.method == 'GET':
         form = CustomRegistrationForm()
@@ -49,11 +55,12 @@ def sign_in(request):
         #     return redirect('home')
     return render(request, 'registration/login.html',{'form':form})
 
+@login_required
 def sign_out(request):
     if request.method == 'POST':
         logout(request)
         return redirect('sign-in')
-    
+  
 def activate_user(request, user_id, token):
     try:
         user = User.objects.get(id=user_id)
@@ -65,3 +72,47 @@ def activate_user(request, user_id, token):
             return HttpResponse("Invalid id or token")
     except User.DoesNotExist:
         return HttpResponse("User not found")
+
+@user_passes_test(is_admin, login_url='no-permission')
+def admin_dashboard(request):
+    users = User.objects.prefetch_related(Prefetch('groups', queryset=Group.objects.all(), to_attr='all_groups')).all()
+    for user in users:
+        if user.all_groups:
+            user.group_name = user.groups.all_groups[0].name
+        else:
+            user.group_name = 'No group assigned'
+    return render(request, 'admin/dashboard.html', {'users':users})
+
+@user_passes_test(is_admin, login_url='no-permission')
+def assign_role(request, user_id):
+    user = User.objects.get(id=user_id)
+    form = AssignRoleForm()
+
+    if request.method == 'POST':
+        form = AssignRoleForm(request.POST)
+        if form.is_valid():
+            role = form.cleaned_data.get('role')
+            user.groups.clear()  # Remove old roles
+            user.groups.add(role)
+            messages.success(request, f"User {
+                             user.username} has been assigned to the {role.name} role")
+            return redirect('admin-dashboard')
+
+    return render(request, 'admin/assign_role.html', {"form": form})
+
+def create_group(request):
+    form = CreateGroupForm()
+    if request.method == 'POST':
+        form = CreateGroupForm(request.POST)
+
+        if form.is_valid():
+            group = form.save()
+            messages.success(request, f"Group {group.name} has been created successfully")
+            return redirect('create-group')
+
+    return render(request, 'admin/create_group.html', {'form':form})
+
+@user_passes_test(is_admin, login_url='no-permission')
+def group_list(request):
+    groups = Group.objects.prefetch_related('permissions').all()
+    return render(request, 'admin/group_list.html', {'groups':groups})
